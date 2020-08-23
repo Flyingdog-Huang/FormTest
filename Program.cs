a420b97ddf9dfbbb577ccdb3f48bf8d284abcc50
unknown <003740@domain.com>
2020-08-23 22:55:31 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTest
+{
+    static class Program
+    {
+        public static bool closeflag = true;
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            if (Program.closeflag == false)
+            {
+                Application.Run(new Form2());
+            }
+        }
+    }
+}
